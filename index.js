0b836a60547d761303a40a60d6125fe0ce69c93e
fredngg <fredngjq@gmail.com>
2024-07-08 18:38:43 +0800

    first commit

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ const bot = new Telegraf(process.env.BOT_TOKEN); // Use your token
 bot.start((ctx) => ctx.reply('Welcome! I am your Telegram bot.'));
 
 // More logic to be added
-
 bot.launch(); 
 
