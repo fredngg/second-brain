a7c6c670d4e6a714997b2919da8784688048b33c
fredngg <fredngjq@gmail.com>
2024-07-08 18:50:11 +0800

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { Telegraf } = require('telegraf');
 require('dotenv').config(); // Load environment variables
 
 const bot = new Telegraf(process.env.BOT_TOKEN); // Use your token
-
 bot.start((ctx) => ctx.reply('Welcome! I am your Telegram bot.'));
 
 // More logic to be added
