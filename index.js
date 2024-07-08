6040ba98c9fb87d5fd1bdfd2cd53ae2356573445
fredngg <fredngjq@gmail.com>
2024-07-08 19:34:11 +0800

    Updated

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const secretPath = `/telegraf/${bot.secretPathComponent()}`;
-bot.telegram.setWebhook(`${process.env.HEROKU_APP_URL}${secretPath}`);
+//bot.telegram.setWebhook(`${process.env.HEROKU_APP_URL}${secretPath}`);
 
 bot.start((ctx) => ctx.reply('Welcome! I am your Telegram bot.'));
 
+bot.launch(); 
+/*
 bot.launch({
     webhook: {
         domain: new URL(process.env.HEROKU_APP_URL).hostname,
         port: process.env.PORT || 8443, // Use the PORT environment variable if available (for Heroku)
     },
-});
+}); */
