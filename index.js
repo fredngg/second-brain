41318b169ab9380d07ed96b141b6750520c8d25a
fredngg <fredngjq@gmail.com>
2024-07-08 19:51:26 +0800

    Update to allow port listening

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
+const express = require('express')
+const expressApp = express()
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const secretPath = `/telegraf/${bot.secretPathComponent()}`;
 //bot.telegram.setWebhook(`${process.env.HEROKU_APP_URL}${secretPath}`);
 
+const port = process.env.PORT || 3000
+expressApp.get('/', (req, res) => {
+  res.send('This is Fred - Second Brain UI.')
+})
+expressApp.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
 bot.start((ctx) => ctx.reply('Welcome! I am your Telegram bot.'));
 
+const PORT = process.env.PORT || 3000;
 bot.launch(); 
 /*
 bot.launch({
-    webhook: {
-        domain: new URL(process.env.HEROKU_APP_URL).hostname,
-        port: process.env.PORT || 8443, // Use the PORT environment variable if available (for Heroku)
-    },
-}); */
+  webhook: {
+    domain: new URL(process.env.HEROKU_APP_URL).hostname,
+    port: PORT,
+  },
+});*/
