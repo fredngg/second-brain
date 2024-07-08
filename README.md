34af05796818d05ad02f6ae5eacf429f5c662e6d
fredngg <fredngjq@gmail.com>
2024-07-08 18:27:11 +0800

    Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A pet project to build a second brain in Telegram with RAG and LLM. 
+Hosted on Heroku.
+
---
+++ index.js
@@ -0,0 +1,11 @@
+const { Telegraf } = require('telegraf');
+require('dotenv').config(); // Load environment variables
+
+const bot = new Telegraf(process.env.BOT_TOKEN); // Use your token
+
+bot.start((ctx) => ctx.reply('Welcome! I am your Telegram bot.'));
+
+// More logic to be added
+
+bot.launch(); 
+
